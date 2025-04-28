eba847dd08525cbb01976ac183709849343070ad
Wygol <rene.strammer@gmx.at>
2025-04-28 11:46:49 +0200

    added another function to the fancyModule.py file module: fancyModule method: modulo_iterable
    also added type description to the functions

--- fancyModule.py
+++ fancyModule.py
@@ -1,4 +1,4 @@
-def generate_fibonacci(n):
+def generate_fibonacci(n: int) -> list:
     """
     Generate the first n Fibonacci numbers as a list.
 
@@ -16,7 +16,7 @@ def generate_fibonacci(n):
 print(generate_fibonacci(20))
 
 
-def square_iterable(iterable):
+def square_iterable(iterable: list) -> list:
     """
     Square each element in the iterable.
 
@@ -24,3 +24,14 @@ def square_iterable(iterable):
     :return: A list containing the squared values.
     """
     return list(map(lambda x: x ** 2, iterable))
+
+
+def modulo_iterable(iterable: list, divisor: int) -> list:
+    """
+    Apply modulo operation to each element in the iterable.
+
+    :param iterable: An iterable (e.g., list, tuple) of numbers.
+    :param divisor: The divisor for the modulo operation.
+    :return: A list containing the results of the modulo operation.
+    """
+    return list(map(lambda x: x % divisor, iterable))
