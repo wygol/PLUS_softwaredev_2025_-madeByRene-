3fdc3b5f85b220f6ee9029d1bf748e9fa9a55641
Wygol <rene.strammer@gmx.at>
2025-04-28 10:42:44 +0200

    tested how python's map function works and how lambda executes with 2 iterable parameters

--- hello.py
+++ hello.py
@@ -1,3 +1,21 @@
 
 if __name__ == "__main__":
-  print("hello")
+    print("hello")
+
+"""
+Just goofing around with the map function
+"""
+
+x = [1, 2, 3, 4, 5]  # iteratable
+y = [2, 2, 2]  # another iterable of different length
+print(type(x))
+
+newX = list(map(lambda x, y: (x % 2)*y, x, y))
+
+
+print(newX)
+
+
+# lambda executes on minimum boundary list length. in this example, the output is [2, 0, 2], since the second parameter Y only has
+# 3 iterable elements, while x has 5 iterable elements.
+# the map function builds an interable with 3 elements.
